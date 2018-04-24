83db7db29090175dd637686059e37e87b66d0890
Valerie Moore <periwinkle17@outlook.com>
2018-04-24 14:10:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ lunch: 'sandwich',
 dinner: 'entree'
 }
 function updateObjectWithKeyAndValue(object, key, value) {
-  return Object.assign({}, recipes, { [cake]: dessert })
+return (recipes, dessert, cake)
 }
