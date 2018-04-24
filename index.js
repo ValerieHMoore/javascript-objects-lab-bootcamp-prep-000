1a71ff9ab1f2f24ba68fd6350b59f5ca973820af
Valerie Moore <periwinkle17@outlook.com>
2018-04-24 13:50:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 var recipes = {
-breakfast: "omelet",
-lunch: "sandwich",
-dinner: "entree"};
-
-function updateObjectWithKeyAndValue(object,key,value) {
-  recipes["dessert"] = "cake";
-  return recipes
+breakfast: 'omelet',
+lunch: 'sandwich',
+dinner: 'entree'
 }
