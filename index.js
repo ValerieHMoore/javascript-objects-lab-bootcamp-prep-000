2054257ee5d64f95813aa00af6e3fd5657703bea
Valerie Moore <periwinkle17@outlook.com>
2018-04-24 16:30:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,6 +13,6 @@ function deleteFromObjectByKey(object, key) {
   return Object
 }
 function destructivelyDeleteFromObjectByKey(object, key) {
-  delete obj.prop;
+  delete newObj.prop;
   return Object
 }
