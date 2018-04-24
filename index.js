57150c669a5960934c2af04ba35f29a2731ded8b
Valerie Moore <periwinkle17@outlook.com>
2018-04-24 13:48:02 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,9 @@
+var recipes = {
+breakfast: "omelet",
+lunch: "sandwich",
+dinner: "entree"};
+
+function updateObjectWithKeyAndValue(object,key,value) {
+  recipes["dessert"] = "cake";
+  return recipes
+}
